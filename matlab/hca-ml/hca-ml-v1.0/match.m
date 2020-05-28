59a698f1d58df34ea6202d343b4172f1a5b44018
Jiangwen (Javon) Sun <JavonSun@users.noreply.github.com>
2020-05-27 22:52:33 -0400

    Update header

--- matlab/hca-ml/hca-ml-v1.0/match.m
+++ matlab/hca-ml/hca-ml-v1.0/match.m
@@ -1,4 +1,4 @@
-% map subjects in s1 to those in s2, if present return the corresponding
+% Match elements of s1 in s2, if present return the corresponding
 % index in s2, otherwise NAN
 
 function map = match(s1, s2)
